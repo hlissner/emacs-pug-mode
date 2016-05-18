791ae67423bf6c8b4fd2a9e5d7769f63fc340263
Henrik Lissner <henrik@lissner.net>
2016-05-18 02:04:35 -0400

    Rename project to pug-mode

--- pug-mode.el
+++ pug-mode.el
@@ -1,4 +1,4 @@
-;;; jaded-mode.el --- Major mode for jade template files
+;;; pug-mode.el --- Major mode for jade template files
 ;;
 ;; Copyright (c) 2007, 2008 Nathan Weizenbaum
 ;; Copyright (c) 2009-2013 Daniel Mendler
@@ -7,7 +7,7 @@
 ;;
 ;; Author: Henrik Lissner
 ;; Inspired by: http://github.com/slim-template/emacs-slim
-;; URL: http://github.com/hlissner/jaded-mode
+;; URL: http://github.com/hlissner/pug-mode
 ;; Version: 1.0
 ;; Keywords: markup, language, jade
 ;;
@@ -25,31 +25,31 @@
 
 ;; User definable variables
 
-(defgroup jaded nil
-  "Support for the Jade template language."
+(defgroup pug nil
+  "Support for the Pug template language."
   :group 'languages
-  :prefix "jaded-")
+  :prefix "pug-")
 
-(defcustom jaded-mode-hook nil
-  "Hook run when entering Jade mode."
+(defcustom pug-mode-hook nil
+  "Hook run when entering Pug mode."
   :type 'hook
-  :group 'jaded)
+  :group 'pug)
 
-(defcustom jaded-backspace-backdents-nesting t
-  "Non-nil to have `jaded-electric-backspace' re-indent all code
+(defcustom pug-backspace-backdents-nesting t
+  "Non-nil to have `pug-electric-backspace' re-indent all code
 nested beneath the backspaced line be re-indented along with the
 line itself."
   :type 'boolean
-  :group 'jaded)
+  :group 'pug)
 
-(defvar jaded-indent-function 'jaded-indent-p
+(defvar pug-indent-function 'pug-indent-p
   "This function should look at the current line and return true
 if the next line could be nested within this line.")
 
 
 ;; Font lock
 
-(defconst jaded-tags-re
+(defconst pug-tags-re
   (concat "^ *\\("
           (regexp-opt
            '("a" "abbr" "acronym" "address" "applet" "area" "article" "aside"
@@ -68,16 +68,16 @@ if the next line could be nested within this line.")
           "\\)")
   "Regex of all html4/5 tags.")
 
-(defconst jaded-selfclosing-tags-re
+(defconst pug-selfclosing-tags-re
   (concat "^ *"
           (regexp-opt
            '("meta" "title" "img" "area" "base" "br" "col" "command" "embed" "hr" "input"
              "link" "param" "source" "track" "wbr") t)))
 
-(defconst jaded-keywords-re
+(defconst pug-keywords-re
   (concat "^ *\\(?:- \\)?" (regexp-opt '("extends" "block") t)))
 
-(defconst jaded-control-re
+(defconst pug-control-re
   (concat "^ *\\(- \\)?\\("
           (regexp-opt
            '("if" "unless" "while" "until" "else" "for"
@@ -88,12 +88,12 @@ if the next line could be nested within this line.")
           "\\)"))
 
 ;; Helper for nested block (comment, embedded, text)
-(defun jaded-nested-re (re)
+(defun pug-nested-re (re)
   (concat "^\\( *\\)" re "\n\\(\\(?:\\1" (make-string tab-width ? ) ".*\\| *\\)\n\\)*"))
 
-(defconst jaded-font-lock-keywords
+(defconst pug-font-lock-keywords
   `(;; comment block
-    (,(jaded-nested-re "//")
+    (,(pug-nested-re "//")
      0 font-lock-comment-face)
     ;; comment line
     ("^ *\\(-//\\|//-?\\).*"
@@ -102,13 +102,13 @@ if the next line could be nested within this line.")
     ("<!--.*-->"
      0 font-lock-comment-face)
     ;; filters
-    (,(jaded-nested-re "\\(:[a-z0-9_]+\\)")
+    (,(pug-nested-re "\\(:[a-z0-9_]+\\)")
      (0 font-lock-preprocessor-face prepend))
     ;; text block
-    ;; (,(jaded-nested-re "[\\.#+a-z][^ \t]*\\(?:(.+)\\)?\\(\\.\\)")
+    ;; (,(pug-nested-re "[\\.#+a-z][^ \t]*\\(?:(.+)\\)?\\(\\.\\)")
     ;;  2 font-lock-string-face)
 
-    (,jaded-control-re
+    (,pug-control-re
      2 font-lock-keyword-face append)
 
     ("^ *|.*"
@@ -139,7 +139,7 @@ if the next line could be nested within this line.")
     ("^ *[a-z0-9_#-]*\\(\\(\\.[a-z0-9_-]+\\((.*)\\)?\\)+\\)[^ \t]*$"
      1 font-lock-variable-name-face append)
     ;; tag
-    (,jaded-tags-re
+    (,pug-tags-re
      1 font-lock-function-name-face)
 
     ;; doctype
@@ -158,11 +158,11 @@ if the next line could be nested within this line.")
      1 font-lock-preprocessor-face)
     ))
 
-(defconst jaded-embedded-re "^ *:[a-z0-9_-]+")
-(defconst jaded-plain-re "^ *[\\.#+a-z][^ \t]*\\(?:(.+)\\)?\\.")
-(defconst jaded-comment-re "^ *-?//-?")
+(defconst pug-embedded-re "^ *:[a-z0-9_-]+")
+(defconst pug-plain-re "^ *[\\.#+a-z][^ \t]*\\(?:(.+)\\)?\\.")
+(defconst pug-comment-re "^ *-?//-?")
 
-(defun* jaded-extend-region ()
+(defun* pug-extend-region ()
   "Extend the font-lock region to encompass embedded engines and comments."
   (let ((old-beg font-lock-beg)
         (old-end font-lock-end))
@@ -170,11 +170,11 @@ if the next line could be nested within this line.")
       (goto-char font-lock-beg)
       (unless (looking-at "\\.$")
         (beginning-of-line)
-        (unless (or (looking-at jaded-embedded-re)
-                    (looking-at jaded-comment-re))
-          (return-from jaded-extend-region)))
+        (unless (or (looking-at pug-embedded-re)
+                    (looking-at pug-comment-re))
+          (return-from pug-extend-region)))
       (setq font-lock-beg (point))
-      (jaded-forward-sexp)
+      (pug-forward-sexp)
       (beginning-of-line)
       (setq font-lock-end (max font-lock-end (point))))
     (or (/= old-beg font-lock-beg)
@@ -202,48 +202,48 @@ declaration"
 
 ;; Mode setup
 
-(defvar jaded-mode-syntax-table
+(defvar pug-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?: "." table)
     (modify-syntax-entry ?_ "w" table)
     table)
-  "Syntax table in use in jaded-mode buffers.")
+  "Syntax table in use in pug-mode buffers.")
 
-(defvar jaded-mode-map
+(defvar pug-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "\177" 'jaded-electric-backspace)
-    (define-key map "\C-?" 'jaded-electric-backspace)
-    (define-key map "\C-c\C-f" 'jaded-forward-sexp)
-    (define-key map "\C-c\C-b" 'jaded-backward-sexp)
-    (define-key map "\C-c\C-u" 'jaded-up-list)
-    (define-key map "\C-c\C-d" 'jaded-down-list)
-    (define-key map "\C-c\C-k" 'jaded-kill-line-and-indent)
+    (define-key map "\177" 'pug-electric-backspace)
+    (define-key map "\C-?" 'pug-electric-backspace)
+    (define-key map "\C-c\C-f" 'pug-forward-sexp)
+    (define-key map "\C-c\C-b" 'pug-backward-sexp)
+    (define-key map "\C-c\C-u" 'pug-up-list)
+    (define-key map "\C-c\C-d" 'pug-down-list)
+    (define-key map "\C-c\C-k" 'pug-kill-line-and-indent)
     map))
 
 ;; For compatibility with Emacs < 24, derive conditionally
-(defalias 'jaded-parent-mode
+(defalias 'pug-parent-mode
   (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
 
 ;;;###autoload
-(define-derived-mode jaded-mode jaded-parent-mode "Jaded"
-  "Major mode for editing Jade files.
+(define-derived-mode pug-mode pug-parent-mode "Pug"
+  "Major mode for editing Pug files.
 
-\\{jaded-mode-map}"
-  (set-syntax-table jaded-mode-syntax-table)
-  (add-to-list 'font-lock-extend-region-functions 'jaded-extend-region)
+\\{pug-mode-map}"
+  (set-syntax-table pug-mode-syntax-table)
+  (add-to-list 'font-lock-extend-region-functions 'pug-extend-region)
   (set (make-local-variable 'font-lock-multiline) t)
-  (set (make-local-variable 'indent-line-function) 'jaded-indent-line)
-  (set (make-local-variable 'indent-region-function) 'jaded-indent-region)
+  (set (make-local-variable 'indent-line-function) 'pug-indent-line)
+  (set (make-local-variable 'indent-region-function) 'pug-indent-region)
   (set (make-local-variable 'parse-sexp-lookup-properties) t)
   (set (make-local-variable 'electric-indent-chars) nil)
   (setq comment-start "/")
   (setq indent-tabs-mode nil)
-  (setq font-lock-defaults '((jaded-font-lock-keywords) nil t)))
+  (setq font-lock-defaults '((pug-font-lock-keywords) nil t)))
 
 ;; Useful functions
 
-(defun jaded-comment-block ()
-  "Comment the current block of Jade code."
+(defun pug-comment-block ()
+  "Comment the current block of Pug code."
   (interactive)
   (save-excursion
     (let ((indent (current-indentation)))
@@ -252,24 +252,24 @@ declaration"
       (newline)
       (indent-to indent)
       (beginning-of-line)
-      (jaded-mark-sexp)
-      (jaded-reindent-region-by tab-width))))
+      (pug-mark-sexp)
+      (pug-reindent-region-by tab-width))))
 
-(defun jaded-uncomment-block ()
-  "Uncomment the current block of Jade code."
+(defun pug-uncomment-block ()
+  "Uncomment the current block of Pug code."
   (interactive)
   (save-excursion
     (beginning-of-line)
-    (while (not (looking-at jaded-comment-re))
-      (jaded-up-list)
+    (while (not (looking-at pug-comment-re))
+      (pug-up-list)
       (beginning-of-line))
-    (jaded-mark-sexp)
+    (pug-mark-sexp)
     (kill-line 1)
-    (jaded-reindent-region-by (- tab-width))))
+    (pug-reindent-region-by (- tab-width))))
 
 ;; Navigation
 
-(defun jaded-forward-through-whitespace (&optional backward)
+(defun pug-forward-through-whitespace (&optional backward)
   "Move the point forward at least one line, until it reaches
 either the end of the buffer or a line with no whitespace.
 
@@ -280,7 +280,7 @@ If `backward' is non-nil, move the point backward instead."
           while (and (not (funcall endp))
                      (looking-at "^[ \t]*$")))))
 
-(defun jaded-at-indent-p ()
+(defun pug-at-indent-p ()
   "Returns whether or not the point is at the first
 non-whitespace character in a line or whitespace preceding that
 character."
@@ -289,74 +289,74 @@ character."
       (back-to-indentation)
       (>= (point) opoint))))
 
-(defun jaded-forward-sexp (&optional arg)
+(defun pug-forward-sexp (&optional arg)
   "Move forward across one nested expression.
 With `arg', do it that many times.  Negative arg -N means move
 backward across N balanced expressions.
 
-A sexp in Jade is defined as a line of Jade code as well as any
+A sexp in Pug is defined as a line of Pug code as well as any
 lines nested beneath it."
   (interactive "p")
   (or arg (setq arg 1))
-  (if (and (< arg 0) (not (jaded-at-indent-p)))
+  (if (and (< arg 0) (not (pug-at-indent-p)))
       (back-to-indentation)
     (while (/= arg 0)
       (let ((indent (current-indentation)))
-        (loop do (jaded-forward-through-whitespace (< arg 0))
+        (loop do (pug-forward-through-whitespace (< arg 0))
               while (and (not (eobp))
                          (not (bobp))
                          (> (current-indentation) indent)))
         (back-to-indentation)
         (setq arg (+ arg (if (> arg 0) -1 1)))))))
 
-(defun jaded-backward-sexp (&optional arg)
+(defun pug-backward-sexp (&optional arg)
   "Move backward across one nested expression.
 With ARG, do it that many times.  Negative arg -N means move
 forward across N balanced expressions.
 
-A sexp in Jade is defined as a line of Jade code as well as any
+A sexp in Pug is defined as a line of Pug code as well as any
 lines nested beneath it."
   (interactive "p")
-  (jaded-forward-sexp (if arg (- arg) -1)))
+  (pug-forward-sexp (if arg (- arg) -1)))
 
-(defun jaded-up-list (&optional arg)
+(defun pug-up-list (&optional arg)
   "Move out of one level of nesting.
 With ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (while (> arg 0)
     (let ((indent (current-indentation)))
-      (loop do (jaded-forward-through-whitespace t)
+      (loop do (pug-forward-through-whitespace t)
             while (and (not (bobp))
                        (>= (current-indentation) indent)))
       (setq arg (- arg 1))))
   (back-to-indentation))
 
-(defun jaded-down-list (&optional arg)
+(defun pug-down-list (&optional arg)
   "Move down one level of nesting.
 With ARG, do this that many times."
   (interactive "p")
   (or arg (setq arg 1))
   (while (> arg 0)
     (let ((indent (current-indentation)))
-      (jaded-forward-through-whitespace)
+      (pug-forward-through-whitespace)
       (when (<= (current-indentation) indent)
-        (jaded-forward-through-whitespace t)
+        (pug-forward-through-whitespace t)
         (back-to-indentation)
         (error "Nothing is nested beneath this line"))
       (setq arg (- arg 1))))
   (back-to-indentation))
 
-(defun jaded-mark-sexp ()
-  "Marks the next Jade block."
-  (let ((forward-sexp-function 'jaded-forward-sexp))
+(defun pug-mark-sexp ()
+  "Marks the next Pug block."
+  (let ((forward-sexp-function 'pug-forward-sexp))
     (mark-sexp)))
 
-(defun jaded-mark-sexp-but-not-next-line ()
-  "Marks the next Jade block, but puts the mark at the end of the
+(defun pug-mark-sexp-but-not-next-line ()
+  "Marks the next Pug block, but puts the mark at the end of the
 last line of the sexp rather than the first non-whitespace
 character of the next line."
-  (jaded-mark-sexp)
+  (pug-mark-sexp)
   (let ((pos-of-end-of-line (save-excursion
                               (goto-char (mark))
                               (end-of-line)
@@ -371,36 +371,36 @@ character of the next line."
 
 ;; Indentation and electric keys
 
-(defun jaded-indent-p ()
+(defun pug-indent-p ()
   "Returns true if the current line can have lines nested beneath it."
-  (or (looking-at jaded-comment-re)
-      (looking-at jaded-embedded-re)
+  (or (looking-at-p pug-comment-re)
+      (looking-at-p pug-embedded-re)
       (and (save-excursion
              (back-to-indentation)
              (not (memq (face-at-point) '(font-lock-preprocessor-face))))
-           (not (looking-at jaded-selfclosing-tags-re))
-           (loop for opener in `(,(concat "^ *\\([\\.#+]\\|" jaded-tags-re "\\)[^ \t]*\\((.+)\\)?\n")
+           (not (looking-at-p pug-selfclosing-tags-re))
+           (loop for opener in `(,(concat "^ *\\([\\.#+]\\|" pug-tags-re "\\)[^ \t]*\\((.+)\\)?\n")
                                  "^ *[\\.#+a-z][^ \t]*\\(?:(.+)\\)?\\.\n"
                                  "^ *[-=].*do[ \t]*\\(|.*|[ \t]*\\)?$"
-                                 ,jaded-control-re)
-                 if (looking-at opener) return t
+                                 ,pug-control-re)
+                 if (looking-at-p opener) return t
                  finally return nil))))
 
-(defun jaded-compute-indentation ()
+(defun pug-compute-indentation ()
   "Calculate the maximum sensible indentation for the current line."
   (save-excursion
     (beginning-of-line)
     (if (bobp) 0
-      (jaded-forward-through-whitespace t)
+      (pug-forward-through-whitespace t)
       (+ (current-indentation)
-         (if (funcall jaded-indent-function)
+         (if (funcall pug-indent-function)
              tab-width
            0)))))
 
-(defun jaded-indent-region (start end)
+(defun pug-indent-region (start end)
   "Indent each nonblank line in the region.
 This is done by indenting the first line based on
-`jaded-compute-indentation' and preserving the relative
+`pug-compute-indentation' and preserving the relative
 indentation of the rest of the region.
 
 If this command is used multiple times in a row, it will cycle
@@ -413,7 +413,7 @@ between possible indentations."
           (next-line-column
            (if (and (equal last-command this-command) (/= (current-indentation) 0))
                (* (/ (- (current-indentation) 1) tab-width) tab-width)
-             (jaded-compute-indentation))))
+             (pug-compute-indentation))))
       (while (< (point) end)
         (setq this-line-column next-line-column
               current-column (current-indentation))
@@ -430,7 +430,7 @@ between possible indentations."
         (forward-line 1)))
     (move-marker end nil)))
 
-(defun jaded-indent-line ()
+(defun pug-indent-line ()
   "Indent the current line.
 The first time this command is used, the line will be indented to the
 maximum sensible indentation.  Each immediately subsequent usage will
@@ -439,7 +439,7 @@ back-dent the line by `tab-width' spaces.  On reaching column
   (interactive "*")
   (let ((ci (current-indentation))
         (cc (current-column))
-        (need (jaded-compute-indentation)))
+        (need (pug-compute-indentation)))
     (save-excursion
       (beginning-of-line)
       (delete-horizontal-space)
@@ -449,7 +449,7 @@ back-dent the line by `tab-width' spaces.  On reaching column
       (if (< (current-column) (current-indentation))
           (forward-to-indentation 0))))
 
-(defun jaded-reindent-region-by (n)
+(defun pug-reindent-region-by (n)
   "Add N spaces to the beginning of each line in the region.
 If N is negative, will remove the spaces instead.  Assumes all
 lines in the region have indentation >= that of the first line."
@@ -459,13 +459,13 @@ lines in the region have indentation >= that of the first line."
       (while (re-search-forward (concat "^" (make-string ci ? )) bound t)
         (replace-match (make-string (max 0 (+ ci n)) ? ) bound nil)))))
 
-(defun jaded-electric-backspace (arg)
+(defun pug-electric-backspace (arg)
   "Delete characters or back-dent the current line.
 If invoked following only whitespace on a line, will back-dent
 the line and all nested lines to the immediately previous
 multiple of `tab-width' spaces.
 
-Set `jaded-backspace-backdents-nesting' to nil to just back-dent
+Set `pug-backspace-backdents-nesting' to nil to just back-dent
 the current line."
   (interactive "*p")
   (if (or (/= (current-indentation) (current-column))
@@ -475,28 +475,28 @@ the current line."
     (save-excursion
       (let ((ci (current-column)))
         (beginning-of-line)
-        (if jaded-backspace-backdents-nesting
-            (jaded-mark-sexp-but-not-next-line)
+        (if pug-backspace-backdents-nesting
+            (pug-mark-sexp-but-not-next-line)
           (set-mark (save-excursion (end-of-line) (point))))
-        (jaded-reindent-region-by (* (- arg) tab-width))
+        (pug-reindent-region-by (* (- arg) tab-width))
         (back-to-indentation)
         (pop-mark)))))
 
-(defun jaded-kill-line-and-indent ()
+(defun pug-kill-line-and-indent ()
   "Kill the current line, and re-indent all lines nested beneath it."
   (interactive)
   (beginning-of-line)
-  (jaded-mark-sexp-but-not-next-line)
+  (pug-mark-sexp-but-not-next-line)
   (kill-line 1)
-  (jaded-reindent-region-by (* -1 tab-width)))
+  (pug-reindent-region-by (* -1 tab-width)))
 
-(defun jaded-indent-string ()
+(defun pug-indent-string ()
   "Return the indentation string for `tab-width'."
   (mapconcat 'identity (make-list tab-width " ") ""))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.jade\\'" . jaded-mode))
+(add-to-list 'auto-mode-alist '("\\.jade\\'" . pug-mode))
 
 ;; Setup/Activation
-(provide 'jaded-mode)
-;;; jaded-mode.el ends here
+(provide 'pug-mode)
+;;; pug-mode.el ends here
